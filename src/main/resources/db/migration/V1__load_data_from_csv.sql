11750d3eaf5e39f25bcbdee24cc61d80c069417d
Iván González <cafeteru.dev@gmail.com>
2023-05-31 01:43:30 +0200

    Create config to Flyway

---
+++ src/main/resources/db/migration/V1__load_data_from_csv.sql
@@ -0,0 +1,36 @@
+CREATE TABLE product
+(
+    id       INT PRIMARY KEY,
+    sequence INT
+);
+
+CREATE TABLE size
+(
+    id         INT PRIMARY KEY,
+    product_id INT,
+    back_soon  BOOLEAN,
+    special    BOOLEAN,
+    FOREIGN KEY (product_id) REFERENCES product (id)
+);
+
+CREATE TABLE stock
+(
+    size_id  INT PRIMARY KEY,
+    quantity INT,
+    FOREIGN KEY (size_id) REFERENCES product (id)
+);
+
+COPY product (id, sequence)
+    FROM 'filesystem:src/main/resources/db/migration/product.csv'
+    DELIMITER ','
+    CSV HEADER;
+
+COPY size (id, product_id, back_soon, special)
+    FROM 'filesystem:src/main/resources/db/migration/size.csv'
+    DELIMITER ','
+    CSV HEADER;
+
+COPY stock (size_id, quantity)
+    FROM 'filesystem:src/main/resources/db/migration/stock.csv'
+    DELIMITER ','
+    CSV HEADER;
