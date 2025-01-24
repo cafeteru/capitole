44d3e495dec3a9bd590b83b75e0aac757a615d4b
Iván González <cafeteru.dev@gmail.com>
2025-01-24 21:51:13 +0100

    Create project

---
+++ src/test/java/io/github/cafeteru/capitole/CapitoleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cafeteru.capitole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
