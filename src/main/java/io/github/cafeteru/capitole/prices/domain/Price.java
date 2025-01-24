2ae052b9579fc8175ee4261f6ec2e8d0285c9696
Iván González <cafeteru.dev@gmail.com>
2025-01-24 23:05:00 +0100

    Create domain

---
+++ src/main/java/io/github/cafeteru/capitole/prices/domain/Price.java
@@ -0,0 +1,25 @@
+package io.github.cafeteru.capitole.prices.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
+public class Price {
+    private Long id;
+    private Integer brandId;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Integer priceList;
+    private Integer productId;
+    private Integer priority;
+    private BigDecimal price;
+    private String curr;
+}
